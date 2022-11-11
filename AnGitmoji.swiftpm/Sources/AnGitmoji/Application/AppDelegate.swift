2f6b58cece15decc54eb662b41e0199c9a4ea968
Jinwoo Kim <kidjinwoo@me.com>
2022-11-11 18:39:48 +0900

    no message

--- AnGitmoji.swiftpm/Sources/AnGitmoji/Application/AppDelegate.swift
+++ AnGitmoji.swiftpm/Sources/AnGitmoji/Application/AppDelegate.swift
@@ -3,7 +3,7 @@ import AnGitmojiObjC
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
 #if os(macOS) || targetEnvironment(macCatalyst)
-    private let s: GitmojiGroupsStatusMenuItem = .init()
+//    private let s: GitmojiGroupsStatusMenuItem = .init()
 #endif
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
